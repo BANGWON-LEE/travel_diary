92b07e856aff9c2c4aacb0b9f29ec32ba1ab1e97
BANGWON-LEE <qkddnjs31@gmail.com>
2023-03-18 19:22:01 +0900

    20230318 카카오 지도 검색 리스트 페이징 처리

--- src/components/ModalRegisterProject.tsx
+++ src/components/ModalRegisterProject.tsx
@@ -297,36 +297,39 @@ const ModalRegisterProject = ({ modalStatus, setModalStatus }: ModalType) => {
 
     // 검색결과 목록 하단에 페이지번호를 표시는 함수입니다
     function displayPagination(pagination: any) {
+      
       let paginationEl = document.getElementById("pagination"),
-        fragment = document.createDocumentFragment(),
-        i;
-
+      fragment = document.createDocumentFragment(),
+      i;
+      
+      console.log('pa', paginationEl?.hasChildNodes())
       // 기존에 추가된 페이지번호를 삭제합니다
       while (paginationEl?.hasChildNodes()) {
         if (paginationEl.lastChild !== null) {
           paginationEl.removeChild(paginationEl.lastChild);
         }
-
-        for (i = 1; i <= pagination.length; i++) {
-          let el = document.createElement("a");
-          el.href = "#";
-          if (el !== null) el.innerHTML = String(i);
-
-          if (i === pagination.current) {
-            el.className = "on";
-          } else {
-            el.onclick = (function (i) {
-              return function () {
-                pagination.gotoPage(i);
-              };
-            })(i);
-          }
-
-          fragment.appendChild(el);
-        }
-        paginationEl?.appendChild(fragment);
       }
+      for (i=1; i<=pagination.last; i++) {
+        var el = document.createElement('a');
+        el.href = "#";
+        
+        el.innerHTML = String(i);
+
+        if (i===pagination.current) {
+            el.className = 'on';
+        } else {
+            el.onclick = (function(i) {
+                return function() {
+                    pagination.gotoPage(i);
+                }
+            })(i);
+        }
+
+        fragment.appendChild(el);
     }
+    paginationEl?.appendChild(fragment);
+}
+
 
     setSearchStatus(false);
   }, [searchStatus]);
