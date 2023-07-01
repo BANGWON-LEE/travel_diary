f7b466c897cff2ffdab420b4348aeb34b9723b56
BANGWON-LEE <qkddnjs31@gmail.com>
2023-07-01 20:01:11 +0900

    컴포넌트 구조 변경

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,14 +1,60 @@
-import { useState, useEffect } from "react";
+import React, { useEffect, useState } from 'react';
 
-const Main :  React.FC  = ()  => {
+import ModalRegisterProject from './ModalRegisterProject';
 
+const Main: React.FC = () => {
+  const [modalStatus, setModalStatus] = useState<boolean>(false);
 
-    return(
-        <div>
-        
+  return (
+    <div>
+      <div className="main_background">
+        <ModalRegisterProject
+          modalStatus={modalStatus}
+          setModalStatus={setModalStatus}
+        />
+        <div className="main_top">
+          <div className="main_top_block">
+            <h1 className="main_top_block_title">Travel Plans</h1>
+          </div>
         </div>
-    )
-
-}
+        <div className="main_bottom">
+          <div className="main_bottom_top_area">
+            <div className="main_bottom_top_area_block">
+              <button
+                type="button"
+                className="main_bottom_top_area_block_btn"
+                onClick={() => setModalStatus(true)}
+              >
+                프로젝트 생성
+              </button>
+            </div>
+          </div>
+          <div className="main_bottom_area">
+            <div className="main_bottom_area_project">
+              <div className="main_bottom_area_project_title_block">
+                <p className="main_bottom_area_project_title_block_text">
+                  힐링 여행 계획
+                </p>
+                <div className="main_bottom_area_project_title_block_loc">
+                  국내
+                </div>
+              </div>
+              <div className="main_bottom_area_project_content_block">
+                <p className="main_bottom_area_project_content_block_date">
+                  2023-02-18 ~ 2023-02-20
+                </p>
+                <p className="main_bottom_area_project_content_block_detail_loc">
+                  부산,김해
+                </p>
+              </div>
+            </div>
+            <div className="main_bottom_area_project" />
+            <div className="main_bottom_area_project" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Main;
