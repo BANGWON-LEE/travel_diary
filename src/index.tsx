3739bea1a4cd8be644e1b70e33a97b730e13506a
BANGWON-LEE <qkddnjs31@gmail.com>
2023-07-01 20:01:28 +0900

    eslint로 교정

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
+import './index.css';
+import './styles/modal.scss';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
