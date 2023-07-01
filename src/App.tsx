f7a8e9c35427198432dbe6bcd8d7895166e78d2d
BANGWON-LEE <qkddnjs31@gmail.com>
2023-07-01 20:00:53 +0900

    eslint로 교정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
 // import './App.css';
-import './styles/main.scss'
+import './styles/main.scss';
+
+import React from 'react';
+
+import logo from './logo.svg';
 import MainPage from './pages/MainPage';
 
 function App() {
--- src/env.js
+++ src/env.js
@@ -1,7 +1,6 @@
 // env.js
 const env = {
-    apiKey: process.env.REACT_APP_KAKAO_JAVASCRIPT_KEY
-  };
-  
-  export default env;
-  
+  apiKey: process.env.REACT_APP_KAKAO_JAVASCRIPT_KEY,
+};
+
+export default env;
