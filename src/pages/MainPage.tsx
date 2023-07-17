b410a9f4f3625992f485e6727692f713c58118d9
BANGWON-LEE <qkddnjs31@gmail.com>
2023-07-17 16:38:48 +0900

    ModalRegisterProject 경로 변경

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import Main from '../components/Main';
-import ModalRegisterProject from '../components/ModalRegisterProject';
+import ModalRegisterProject from '../components/popup/ModalRegisterProject';
 
 const MainPage: React.FC = () => {
   return <Main />;
