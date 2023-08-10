7e1f86c7a870c1c72c35a6552fde7894662e7e2f
BANGWON-LEE <qkddnjs31@gmail.com>
2023-08-10 16:52:38 +0900

    프로젝트 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@
 import './styles/main.scss';
 
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import logo from './logo.svg';
 import MainPage from './pages/MainPage';
+import ProjectPage from './pages/ProjectPage';
 
 function App() {
   return (
-    <div className="App">
-      <MainPage />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/project" element={<ProjectPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
