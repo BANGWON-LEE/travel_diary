e4e5c12d514e8e757072e3e8ed4d5a2a53d958cb
BANGWON-LEE <qkddnjs31@gmail.com>
2024-01-04 20:13:08 +0900

    fix : 텍스트 변경

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -7,15 +7,15 @@ const Main: React.FC = () => {
       <div className="main_background">
         <div className="main_top">
           <div className="main_top_block">
-            <h1 className="main_top_block_title">Travel Diary</h1>
+            <h1 className="main_top_block_title">나는 작가입니다.</h1>
           </div>
         </div>
         <div className="main_bottom">
           <div className="main_project_intro">
             <p>
-              당신이 여행 경험을 공유해주시면,
-              <br /> 여행기에 어울리는 <br />
-              음악을 소개해드립니다.
+              당신이 경험을 공유해주시면,
+              <br /> 경험과 어울리는 인물을 주인공으로 <br />
+              글을 작성해 드립니다.
             </p>
           </div>
           <div className="main_bottom_top_area">
